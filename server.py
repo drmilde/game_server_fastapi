b9f455d960c0d5c9645d452f6c1c9a05ac61c93e
Jan-Torsten Milde <milde@hs-fulda.de>
2026-01-14 09:53:45 +0100

    Add files via upload

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from pydantic import BaseModel
 import uvicorn
 
@@ -15,6 +15,23 @@ class User(BaseModel):
     id: int
     name: str
 
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+manager = ConnectionManager()
+
 ticker = 0
 users = []
 messages = []
@@ -22,16 +39,17 @@ messages = []
 current_user = 0
 user_ids = [1,2,4,8,16,32,64,128]
 
-def tick():
+async def tick():
     global ticker
     ticker = ticker + 1
+    await manager.broadcast(f"tick: {ticker}")
 
-def addUser(user: User):
+async def addUser(user: User):
     global current_user
     if current_user < 8:
         users.append(user)
         current_user = current_user + 1
-    tick()
+    await tick()
 
 def clearUsers():
     global current_user
@@ -48,11 +66,11 @@ def resetServer():
     clearUsers()
     clearMessages()
 
-def addEntry(entry: Entry):
+async def addEntry(entry: Entry):
     messages.append(entry)
-    tick()
+    await tick()
 
-def getEntries(id: int):
+async def getEntries(id: int):
     # TODO check if message is for id
     result = []
     for entry in messages:
@@ -61,7 +79,7 @@ def getEntries(id: int):
             result.append(entry)
             entry.t = max(0,entry.t - id)
     tidyMessages()
-    tick()
+    await tick()
     return(result)
 
 def tidyMessages():
@@ -83,7 +101,7 @@ async def info():
 async def join(name:str):
     if current_user < 8:
         user = User(id=user_ids[current_user], name=name)
-        addUser(user)
+        await addUser(user)
         return {"id": user.id, "name": name, "m": "joined"}
     else:
         return {"m": "cannot join, max number of users reached"}
@@ -102,7 +120,7 @@ async def user_list():
 @app.post("/s/{id}/{r}")
 async def send(id: int, r:int, m: Message):
     entry = Entry(t=r, f=id, b=ticker, m = m.m)
-    addEntry(entry)
+    await addEntry(entry)
     return {
         "id": id,
         "a": r,
@@ -113,7 +131,7 @@ async def send(id: int, r:int, m: Message):
 async def message_list(id: int):
     return {
         "id": id,
-        "messages": getEntries(id)
+        "messages": await getEntries(id)
         }
 
 @app.get("/all")
@@ -132,6 +150,19 @@ async def clear_messages():
 @app.get("/r")
 async def reset():
     return {"m": "reset"}
+
+# Websocket
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.broadcast(f"Someone said: {data}")
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        print("Client disconnected")
 
 
 if __name__ == "__main__":
